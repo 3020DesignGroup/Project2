c327504489575d70efb01def654585092a5b9af7
3020DesignGroup <WETM3020Design@yahoo.com>
2016-02-29 14:54:04 -0700

    Fully Functional Final
    All done with  GraphDriver.cpp

--- Project2/GraphDriver.cpp
+++ Project2/GraphDriver.cpp
@@ -23,106 +23,136 @@ bool getInput(Graph& graph);
 void randBuildGraph(Graph& graph, int size);
 void fileBuildGraph(Graph& graph, ifstream& infile);
 string intToString(int intConvert);
+void printGraph(Graph graph);
 
 int main()
 {
-    Graph graph;
-    int longestPath;
-    bool going = getInput(graph);
-    while (!going)
-    {
-        going = getInput(graph);
-    }
+	Graph graph;
+	int longestPath;
+	bool going = getInput(graph);
+	while (!going)
+	{
+		going = getInput(graph);
+	}
 
-    Timer timer;
-    timer.start();
-    longestPath = graph.getLongestPath();
-    timer.stop();
+	Timer timer;
+	timer.start();
+	longestPath = graph.getLongestPath();
+	timer.stop();
 
-    cout << "Longest Path in Tree is: " << longestPath << "\nand was found in " << timer() << " seconds" << endl;
+	cout << "Longest Path in Tree is: " << longestPath << "\nand was found in " << timer() << " seconds" << endl;
 
-    return 0;
+	//Debug function
+	printGraph(graph);
+	
+	return 0;
 }
 
 bool getInput(Graph& graph)
 {
-    int size;
-    string filename;
-    ifstream infile;
-    string option;
+	int size;
+	string filename;
+	ifstream infile;
+	string option;
+	randomizeSeed();
 
-    //prompt for random or file entry.
-    cout << "Enter r for random filling of the tree. Enter f for input via external file: ";
-    cin >> option;
-    if (option == "r")
-    {
-        //prompt for size
-        cout << "please enter desired size: ";
-        cin >> size;
+	//prompt for random or file entry.
+	cout << "Enter r for random filling of the tree. Enter f for input via external file: ";
+	cin >> option;
+	if (option == "r")
+	{
+		//prompt for size
+		cout << "please enter desired size: ";
+		cin >> size;
 
-        randBuildGraph(graph, size);
-        return true;
-    }
+		randBuildGraph(graph, size);
+		return true;
+	}
 
-    // fill the graph by file input
-    else if (option == "f")
-    {
-        //prompt for file
-        cout << "enter filename: ";
-        cin >> filename;
+	// fill the graph by file input
+	else if (option == "f")
+	{
+		//prompt for file
+		cout << "enter filename: ";
+		cin >> filename;
 
-        //open file
-        infile.open(filename);
-        if (!(infile.is_open()))
-        {
-            cerr << "File not found!" << endl << endl;
-            return false;
-        }
+		//open file
+		infile.open(filename);
+		if (!(infile.is_open()))
+		{
+			cerr << "File not found!" << endl << endl;
+			return false;
+		}
 
-        fileBuildGraph(graph, infile);
-        return true;
-    }
-    else
-    {
-        cerr << "unrecognized input. Please enter \"r\" or \"f\"" << endl << endl;
-        return false;
-    }
-    return true;
+		fileBuildGraph(graph, infile);
+		return true;
+	}
+	else
+	{
+		cerr << "unrecognized input. Please enter \"r\" or \"f\"" << endl << endl;
+		return false;
+	}
+	return true;
 }
 
 void randBuildGraph(Graph& graph, int size)
 {
-    int edgeIndex = 0;
-    for(int i = 0; i < size - 1; i++)
-    {
-        Vertex vertex(intToString(i));
-        int numVertices = randReal(0, size - edgeIndex);
-        for(int k = 0; k < numVertices; k++)
-        {
-            vertex.addEdge(edgeIndex);
-            edgeIndex++;
-        }
-        graph.addVertex(vertex);
-    }
-    
-    Vertex vertex(intToString(size-1));
-    while(edgeIndex < size-1)
-    {
-        vertex.addEdge(edgeIndex);
-        edgeIndex++;
-    }
-    graph.addVertex(vertex);
-   
+	int edgeIndex = 0;
+	for (int i = 0; i < size; i++)
+	{
+		Vertex vertex(intToString(i));
+		int numVertices = randInt(0, size - edgeIndex + 1);
+		for (int k = 0; k < numVertices; k++)
+		{
+			if(edgeIndex > i)
+				vertex.addEdge(edgeIndex);
+			edgeIndex++;
+		}
+		graph.addVertex(vertex);
+	}
 }
 
 void fileBuildGraph(Graph& graph, ifstream& infile)
 {
-    
+	int size;
+	infile >> size;
+	int data = 0;
+	string name;
+	for (int i = 0; i < size; i++)
+	{
+		infile >> name;
+		Vertex vertex(name);
+		infile >> data;
+		while (data != -1)
+		{
+			vertex.addEdge(data);
+			infile >> data;
+		}
+		graph.addVertex(vertex);
+	}
+
+
+
 }
 
 string intToString(int intConvert)
 {
-		std::ostringstream cstr;  //create the stream
-		cstr << intConvert;  //put integer into the stream
-		return cstr.str();  //put out the string
+	std::ostringstream cstr;  //create the stream
+	cstr << intConvert;  //put integer into the stream
+	return cstr.str();  //put out the string
+}
+
+void printGraph(Graph graph)
+{
+	vector<Vertex> vertices = graph.getVertices();
+	for (int i = 0; i < vertices.size(); i++)
+	{
+		cout << vertices[i].getName() << "\t";
+		vector<int> edges = vertices[i].getEdges();
+		for (int k = 0; k < edges.size(); k++)
+		{
+			cout << " " << edges[k] << " ";
+		}
+		cout << endl;
+	}
 }
