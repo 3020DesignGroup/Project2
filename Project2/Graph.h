d22c54947de5d8351ed95fcdd1ebbefbc7791725
Wemery <WETM3020Design@yahoo.com>
2016-03-01 20:27:14 -0700

    Fixed more problems

--- Project2/Graph.h
+++ Project2/Graph.h
@@ -15,19 +15,20 @@ class Vertex
 {
 public:
 
-    Vertex();
+	Vertex();
+	Vertex(string name);
 
-    //Returns the vector of Edges in this Vertex
-    vector<int> getEdges() const;
+	//Returns the vector of Edges in this Vertex
+	vector<int> getEdges() const;
 
-    string getName() const;
+	string getName() const;
 
-    void addEdge(int vertex);
+	void addEdge(int vertex);
 
 private:
-    //contains the indeces of the vertices that this Vertex has an edge with
-    vector<int> _edges;
-    string _name;
+	//contains the indeces of the vertices that this Vertex has an edge with
+	vector<int> _edges;
+	string _name;
 };
 
 
@@ -38,79 +39,158 @@ private:
 class Graph
 {
 public:
-    Graph();
-    //added a destructor to handle dynamically constructed Vertices
-    ~Graph();
+	Graph();
 
-    int getLongestPath() const;
-    vector<Vertex> getVertices() const;
+	int getLongestPath() const;
+	vector<Vertex> getVertices() const;
 
-    void addVertex(Vertex vertex);
+	void addVertex(Vertex vertex);
 
-    //Adds an Edge between the specified vertices
-    //Notice order of indeces does not matter due to undirection
-    void addEdge(Vertex vertex1, Vertex vertex2);
+	//Adds an Edge between the specified vertices
+	//Notice order of indeces does not matter due to undirection
+	void addEdge(Vertex vertex1, Vertex vertex2);
 
-    //Adds an edge between the specified indexes of vertices
-    //Notice order of indeces does not matter due to undirection
-    void addEdge(int index1, int index2);
+	//Adds an edge between the specified indexes of vertices
+	//Notice order of indeces does not matter due to undirection
+	void addEdge(int index1, int index2);
 
 private:
-    vector<Vertex> _vertices;
+	vector<Vertex> _vertices;
+	//returns the index of the vertex passed if it exists in the 
+	//_vertices vector. If it does not exist -1 is returned. O(n)
+	//Uses the vertex name and edges to determine equality
+	int findVertex(Vertex vertex);
+	int getLongestPath(int next) const;
 };
 
 //Vertex Function Definitions
 Vertex::Vertex()
 {
+	_name = "Default Name";
+}
 
+Vertex::Vertex(string name)
+{
+	_name = name;
 }
 
 vector<int> Vertex::getEdges() const
 {
-        return _edges;
+	return _edges;
 }
 
 string Vertex::getName() const
 {
-        return _name;
+	return _name;
 }
 
 void Vertex::addEdge(int vertex)
 {
-       
+	if (vertex >= 0)
+		_edges.push_back(vertex);
 }
 
 //Graph Function Definitions
 
 Graph::Graph()
 {
-
-}
-
-Graph::~Graph()
-{
-
+	//nothing needs to be done
 }
 
 int Graph::getLongestPath() const
 {
-    return 0;
+    int max = 0;
+    int height = 0;
+
+    for (int i = 0; i < _vertices.size(); i++)
+    {
+        int height = getLongestPath(i);
+        if (height > max)
+        {
+            max = height;
+        }
+    }
+    return max;
+	
 }
 
 vector<Vertex> Graph::getVertices() const
 {
-    return _vertices;
+	return _vertices;
 }
 
 void Graph::addVertex(Vertex vertex)
 {
-
+	_vertices.push_back(vertex);
 }
 void Graph::addEdge(Vertex vertex1, Vertex vertex2)
 {
-
+	int index = findVertex(vertex2);
+	if (index > -1)
+	{
+		vertex1.addEdge(index);
+	}
 }
 void Graph::addEdge(int index1, int index2)
 {
+	if (index1 < _vertices.size() && index1 >= 0)
+	{
+		Vertex vertex = _vertices[index1];
+		vertex.addEdge(index1);
+	}
+}
 
+//Private Helper Functions
+
+int Graph::getLongestPath(int next) const
+{
+    int max = 0;
+    vector<int> edges = _vertices[next].getEdges();
+	if (edges.empty()) 
+	{
+		return 1;
+	}
+	else
+	{
+		
+		for (int x = 0; x < edges.size(); x++)
+		{
+			int height = getLongestPath(edges[x]);
+			if (height > max)
+			{
+				max = height;
+			}
+		}
+		
+	}
+    return (max + 1);
+
+}
+
+int Graph::findVertex(Vertex vertex)
+{
+	for (int i = 0; i < _vertices.size(); i++)
+	{
+		//Using names and edges of vertices to determine equality
+		if (_vertices[i].getName() == vertex.getName())
+		{
+			vector<int> edges1 = _vertices[i].getEdges();
+			vector<int> edges2 = vertex.getEdges();
+			bool looking = false;
+			int i = 0;
+			while (looking && i < edges2.size())
+			{
+				if (edges1[i] != edges2[i])
+				{
+					looking = false;
+				}
+				i++;
+			}
+			if (looking == true)
+			{
+				return i;
+			}
+		}
+	}
+	return -1;
 }
